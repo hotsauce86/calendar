8a81ff111da4a4fb25cabfce615b46d2688baf72
hotsauce86 <timothy_tierney@my.uri.edu>
2019-08-01 00:39:09 -0400

    fixed bug with using datetime getting stuck in a loop as the var never changed

--- ConsoleAppCal/Program.cs
+++ ConsoleAppCal/Program.cs
@@ -8,9 +8,12 @@ namespace ConsoleAppCal
 {
     class Program
     {
+        static List<DateTime> holidays = new List<DateTime>();
+
+       
         static void Main(string[] args)
         {
-
+            holidays.Add( new DateTime(2019, 1, 1, 4, 0, 14) );
             Console.WriteLine("hello world!");
 
 
@@ -44,16 +47,70 @@ namespace ConsoleAppCal
             DateTime date1 = DateTime.Now;
             DateTime date2 = date1.AddDays(30);
 
+            Console.WriteLine();
+            Console.Write(date1);
+            Console.WriteLine("   " + date1.DayOfWeek);
 
-            Console.WriteLine(date1);
-
-            Console.WriteLine(date2);
+            Console.Write(date2);
+            Console.WriteLine("   " + date2.DayOfWeek);
 
 
             Console.WriteLine("hello, you may type anything to continue");
             string name = Console.ReadLine();
+
+
+            dts(date1, date2);
+
+
+            Console.WriteLine("it worked");
+           name = Console.ReadLine();
         }
+
+        public static DateTime dts(DateTime t1, DateTime t2)
+        {
+            //we want t1 to be at least t2
+            int value = DateTime.Compare(t1, t2);
+            //Console.WriteLine(t1 + "     " + t2);
+            //if(value < 0)
+            //{
+            //t1 = dts(t1.AddDays(1), t2);
+            
+            //}
+
+            while(value < 0)
+            {
+               t1 = t1.AddDays(1);
+                value = DateTime.Compare(t1, t2);
+                Console.WriteLine(value + "  " + t1);
+            }
+            Console.WriteLine(t1 + "     " + t2);
+
+
+            //next we want to check if it lands on a holiday
+            //(assuming the list of holidays is sorted)
+            foreach (var date in holidays)
+            {
+                int value2 = DateTime.Compare(t1, date);
+                Console.WriteLine(t1 + "     " + t2);
+                if (value == 0)
+                {
+                    t1.AddDays(1);
+                }
+            }   
+
+
+
+
+
+
+            
+
+            return t1;
+        }
+
+
     }
+
 
     interface calendar
     {
@@ -62,32 +119,13 @@ namespace ConsoleAppCal
     }
     public class scheduler : calendar
     {
-        // DateTime date1 = new DateTime(2015, 12, 25);
-
-        //DateTime date2 = new DateTime(2015, 12, 29);
-
-
-
-        /*
-         * TEST PRINT FOR DATETIME
-         * 
-         * Console.WriteLine(date1.ToString());
-         * 
-         */
         int start = 1;
 
         int end = 5;
-
-
-
         public void Log(string message)
         {
             Console.WriteLine(message);
         }
-        
-        /*
-         * sample version of rescheduling an event
-         */
 
         public void checkSchedule(int day1, int day2)
         {
